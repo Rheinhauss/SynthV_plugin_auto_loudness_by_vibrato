58e65a192b14c0e353853a037d1bfb736a909950
Rheinhauss <20496246+Rheinhauss@users.noreply.github.com>
2021-02-27 15:24:25 +0800

    add i18n

--- auto_loudness_by_vibrato.js
+++ auto_loudness_by_vibrato.js
@@ -1,11 +1,33 @@
 function getClientInfo() {
     return {
-        "name": "auto loudness by vibrato",
+        "name": SV.T("auto loudness by vibrato"),
         "category": "tool",
         "author": "Rin von Rheinhauss",
         "versionNumber": 1,
         "minEditorVersion": 66049
     };
+}
+function getTranslations(langCode) {
+    if (langCode == "zh-cn") {
+        return [
+            // getClientInfo()
+            ["auto loudness by vibrato", "根据颤音设置响度"],
+
+            // warning_save()
+            ["auto loudness by vibrato: WARNING", "根据颤音设置响度：警告"],
+            ["This script may override your own loudness parameters!\nPlease save or backup the project file in advance!",
+                "本脚本可能会覆盖您的响度参数！\n请预先保存或备份工程文件！"],
+
+            // main()
+
+            
+            //
+            
+            ["Please enter a number here:", "请输入一个数字："],
+            ["Please enter some text here:", "请输入一段文本："]
+        ];
+    }
+    return [];
 }
 
 function main() {
@@ -14,7 +36,6 @@ function main() {
 }
 function warning_save() {
     SV.showMessageBox("auto loudness by vibrato: WARNING",
-        "This script may override your own loudness parameters!\n\
-         Please save or backup the project file in advance!");
+        SV.T("This script may override your own loudness parameters!\nPlease save or backup the project file in advance!"));
     SV.finish();
 }
