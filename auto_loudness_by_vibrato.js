ea0e771105625162b0bd4ea33da80841d48225ae
Rheinhauss <20496246+Rheinhauss@users.noreply.github.com>
2021-02-27 15:13:26 +0800

    add warning_save

--- auto_loudness_by_vibrato.js
+++ auto_loudness_by_vibrato.js
@@ -1,14 +1,18 @@
 function getClientInfo() {
     return {
-      "name" : "auto loudness by vibrato",
-      "category" : "tool",
-      "author" : "Rin von Rheinhauss",
-      "versionNumber" : 1,
-      "minEditorVersion" : 66049
+        "name": "auto loudness by vibrato",
+        "category": "tool",
+        "author": "Rin von Rheinhauss",
+        "versionNumber": 1,
+        "minEditorVersion": 66049
     };
-  }
-  
-  function main() {
-    SV.showMessageBox("My Script", "Hello, world!");
+}
+
+function main() {
+    warning_save();
     SV.finish();
-  }
+}
+function warning_save(){
+    SV.showMessageBox("auto loudness by vibrato: WARNING", "Please save the project file in advance!\n This script may override your own loudness parameters!");
+    SV.finish();
+}
