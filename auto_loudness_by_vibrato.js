b903890cbf9214b27ed1a2d414b6bddbaf1fcad9
Rheinhauss <20496246+Rheinhauss@users.noreply.github.com>
2021-02-27 15:01:58 +0800

    Hello world

---
+++ auto_loudness_by_vibrato.js
@@ -0,0 +1,14 @@
+function getClientInfo() {
+    return {
+      "name" : "auto loudness by vibrato",
+      "category" : "tool",
+      "author" : "Rin von Rheinhauss",
+      "versionNumber" : 1,
+      "minEditorVersion" : 66049
+    };
+  }
+  
+  function main() {
+    SV.showMessageBox("My Script", "Hello, world!");
+    SV.finish();
+  }
